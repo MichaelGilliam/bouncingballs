7b4e67a98799b0e4bb35eb67dcee439156408f08
michaelgilliam.se <michaelgilliam.se@gmail.com>
2021-09-19 22:37:56 -0400

    added code to js

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+// makes the shape, color size, and speed of the ball
 
 function Ball(x, y, velX, velY, color, size) {
   this.x = x;
@@ -20,4 +21,41 @@ function Ball(x, y, velX, velY, color, size) {
   this.velY = velY;
   this.color = color;
   this.size = size;
+}
+// drawing the ball 
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+// new ball added
+
+let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+testBall.x
+testBall.size
+testBall.color
+testBall.draw()
+
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
 }
